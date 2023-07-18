f7d0c309247e75ffd65ba2fce8fd768c5f9fd48d
Darkowns <propaltalt@gmail.com>
2023-07-18 17:20:00 -0300

    Commit 23/07/18

---
+++ Fácil/1.TwoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> m = new HashMap<Integer,Integer>();
+        int[]result = new int[2];
+        
+       for(int i = 0; i < nums.length;i++){
+           int atual = nums[i];
+            int valorProcurar = target - atual;
+            if(m.containsKey(valorProcurar)){
+                result[0]= m.get(valorProcurar);
+                result[1]=i;
+                return result;
+                
+                // return new int[]{numMap.get(complement), i};
+            }
+            m.put(atual,i);
+       }
+       return new int[]{};
+
+    }
+}
